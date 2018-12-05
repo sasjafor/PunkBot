4261f8453ecba87979a901c0911bb528bce4393c
sasjafor <john93412@gmail.com>
2018-12-05 19:03:54 +0100

    updated run script

--- src/run.sh
+++ src/run.sh
@@ -2,5 +2,5 @@
 rm -rf /config/node_modules
 cp -r /usr/src/app/* /config/
 while true; do
-AUTH_TOKEN=${DISCORD_APP_AUTH_TOKEN} node /config/bot.js
+AUTH_TOKEN=${DISCORD_APP_AUTH_TOKEN} YOUTUBE_API_KEY=${YOUTUBE_API_KEY} node /config/bot.js
 done
