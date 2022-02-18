4b2c1587ca1330270a61a1cf25074d07f394e1d2
Sascha Forster <forstesa@student.ethz.ch>
2022-02-18 15:10:58 +0100

    add more rules to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,64 @@
 module.exports = {
     'env': {
         'es2021': true,
-        'node': true
+        'node': true,
     },
     'extends': 'eslint:recommended',
     'parserOptions': {
         'ecmaVersion': 2021,
-        'sourceType': 'module'
+        'sourceType': 'module',
     },
     'rules': {
-        'indent': [
-            'warn',
-            4,
-            { 'SwitchCase': 1 }
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'always'
-        ],
         'brace-style': [
             'error',
             '1tbs',
-            { 'allowSingleLine': true }
+            { 'allowSingleLine': true },
         ],
-        'curly': 'error',
         'camelcase': [
             'warn',
-        ]
-    }
+        ],
+        'comma-dangle': [
+            'warn',
+            'always-multiline',
+        ],
+        'curly': [
+            'warn',
+            'all',
+        ],
+        'eqeqeq': [
+            'error',
+            'always',
+            {
+                'null': 'always',
+            },
+        ],
+        'indent': [
+            'warn',
+            4,
+            { 
+                'SwitchCase': 1,
+                'ArrayExpression': 'first',
+                'ImportDeclaration': 'first',
+                'ObjectExpression': 'first',
+            },
+        ],
+        'linebreak-style': [
+            'error',
+            'unix',
+        ],
+        'quotes': [
+            'error',
+            'single',
+        ],
+        'semi': [
+            'error',
+            'always',
+        ],
+        'sort-imports': [
+            'warn',
+        ],
+        'sort-vars': [
+            'warn',
+        ],
+    },
 };
