0318102525aefe7fb61645d1d9065d6931f121f2
Sascha Forster <forstesa@student.ethz.ch>
2022-02-24 17:32:12 +0100

    switch to jest

--- test/test.js
+++ test/test.js
@@ -1,12 +1,14 @@
-import chai from 'chai';
-import chaiAsPromised from 'chai-as-promised';
-import sinon from 'sinon';
+// import chai from 'chai';
+// import chaiAsPromised from 'chai-as-promised';
+// import sinon from 'sinon';
 
-chai.config.includeStack = true;
-chai.use(chaiAsPromised);
+// chai.config.includeStack = true;
+// chai.use(chaiAsPromised);
 
-const assert = chai.assert;
-const expect = chai.expect;
+// sinon.assert.expose(chai.assert, { prefix: '' });
+
+// const assert = chai.assert;
+// const expect = chai.expect;
 
 import { strings } from '../src/lib/strings.js';
 
@@ -18,28 +20,37 @@ describe('commands', function () {
             guild: {
                 id: 1234,
             },
-            reply: sinon.fake(),
+            reply: jest.fn(),
         };
 
         const player = {
             conn: 'Legit Connection',
-            clear: sinon.fake(),
+            clear: jest.fn(),
         };
 
         const players = {
             1234: player,
         };
 
-        it('returns successfully', function () {
-            return assert.isFulfilled(clear.execute(interaction, players));
+        // it('returns successfully', function () {
+        //     // return assert.isFulfilled(clear.execute(interaction, players));
+            
+        // });
+
+        it('calls clear once', function() {
+            // assert(player.clear.calledOnce);
+            clear.execute(interaction, players);
+            expect(player.clear).toHaveBeenCalledTimes(1);
         });
 
-        it('calls clear once', function () {
-            assert(player.clear.calledOnce);
-        });
+        // it('replies with strings.cleared', function () {
+        //     assert(interaction.reply.calledWith({ content: strings.cleared }));
+        // });
 
-        it('replies with strings.cleared', function () {
-            assert(interaction.reply.calledWith({ content: strings.cleared }));
+        it('replies not connected if conn is undefined or null', function() {
+            player.conn = null;
+            clear.execute(interaction, players);
+            expect(interaction.reply).toHaveBeenCalledWith({ content: strings.notConnected, ephemeral: true });
         });
     });
 });
