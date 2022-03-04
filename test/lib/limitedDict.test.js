e3cf21d878038083e9f3cf1ff0e46d1b1e9d63cc
Sascha Forster <forstesa@student.ethz.ch>
2022-03-04 21:10:14 +0100

    cleanup limited dict test

--- test/lib/limitedDict.test.js
+++ test/lib/limitedDict.test.js
@@ -5,8 +5,6 @@ global.console.log = jest.fn();
 describe('lib', function () {
     describe('limitedDict', function () {
         var ldict = new LimitedDict(3);
-        // const ldictEmptyOrig = new LimitedDict(3);
-        // var ldictEmpty;
 
         const key1 = 'test1';
         const item1 = { value: '', next: null, prev: null };
@@ -16,28 +14,9 @@ describe('lib', function () {
         const item3 = { value: '', next: null, prev: null };
         const key4 = 'test4';
         const item4 = { value: '', next: null, prev: null };
-        // item1.next = item2;
-        // item2.prev = item1;
-        // item2.next = item3;
-        // item3.prev = item2;
-
-        // const dict = {
-        //     key1: item1,
-        //     key2: item2,
-        //     key3: item3,
-        // };
-        // var ldictFull
-        // const ldictFull = new LimitedDict(3);
-        // ldictFull.dict = dict;
-        // ldictFull.size = 3;
-        // ldictFull.head = item1;
-        // ldictFull.tail = item3;
 
         beforeEach(() => {
             jest.clearAllMocks();
-
-            // Object.assign(ldictEmpty, ldictEmptyOrig);
-            // Object.assign()
         });
 
         it('getSize', function() {
