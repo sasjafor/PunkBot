203ab3b9932b79292094726a881ee7febae3f68a
Sascha Forster <forstesa@student.ethz.ch>
2022-03-14 11:39:14 +0100

    fix file bug in docker run script

--- src/run.sh
+++ src/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 rm -rf /config/node_modules
+rm -rf /config/lib
+rm -rf /config/commands
 cp -r /usr/src/app/* /config/
 while true; do
 YOUTUBE_DL_DIR=/usr/local/bin/youtube-dl DISCORD_APP_AUTH_TOKEN=${DISCORD_APP_AUTH_TOKEN} YOUTUBE_API_KEY=${YOUTUBE_API_KEY} node /config/bot.js
