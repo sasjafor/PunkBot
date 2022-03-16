7306a00e02cea50611325cbce942249467a235f0
Sascha Forster <forstesa@student.ethz.ch>
2022-03-16 10:24:30 +0100

    preliminary tests for player

---
+++ test/lib/player.test.js
@@ -0,0 +1,22 @@
+import * as player from '../../src/lib/player.js';
+
+import got from 'got';
+jest.mock('got');
+
+global.console.log = jest.fn();
+
+describe('lib', function () {
+    describe('player', function () {
+        var playerObj = new player.Player();
+
+
+        beforeEach(() => {
+            jest.clearAllMocks();
+        });
+
+        it('getQueue', function() {
+            let queue = playerObj.getQueue();
+            expect(queue).toBe(playerObj.queue);
+        });
+    });
+});
