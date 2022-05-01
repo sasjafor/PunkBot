57171aaaf1b6ac920018028f8d5556ef5cd9f101
Sascha Forster <forstesa@student.ethz.ch>
2022-05-01 16:42:46 +0200

    set debug variable

--- src/run.sh
+++ src/run.sh
@@ -4,5 +4,5 @@ rm -rf /config/lib
 rm -rf /config/commands
 cp -r /usr/src/app/* /config/
 while true; do
-YOUTUBE_DL_DIR=/usr/local/bin/youtube-dl DISCORD_APP_AUTH_TOKEN=${DISCORD_APP_AUTH_TOKEN} YOUTUBE_API_KEY=${YOUTUBE_API_KEY} node /config/bot.js
+DEBUG=info YOUTUBE_DL_DIR=/usr/local/bin/youtube-dl DISCORD_APP_AUTH_TOKEN=${DISCORD_APP_AUTH_TOKEN} YOUTUBE_API_KEY=${YOUTUBE_API_KEY} node /config/bot.js
 done
