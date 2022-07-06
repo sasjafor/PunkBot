25df85a4c8624e0de355f743c9114a8c2fcc77ec
Sascha Forster <forstesa@student.ethz.ch>
2022-07-06 15:08:55 +0200

    run bot in correct directory

--- src/run.sh
+++ src/run.sh
@@ -3,7 +3,8 @@ rm -rf /config/node_modules
 rm -rf /config/lib
 rm -rf /config/commands
 cp -r /usr/src/app/* /config/
+cd /config
 
 while true; do
-    LOG_COLOR=${LOG_COLOR:-'false'} DEBUG=${DEBUG:-'info'} YOUTUBE_DL_DIR=/usr/local/bin/youtube-dl DISCORD_APP_AUTH_TOKEN=${DISCORD_APP_AUTH_TOKEN} YOUTUBE_API_KEY=${YOUTUBE_API_KEY} node /config/bot.js
+    LOG_COLOR=${LOG_COLOR:-'false'} DEBUG=${DEBUG:-'info'} YOUTUBE_DL_DIR=/usr/local/bin/youtube-dl DISCORD_APP_AUTH_TOKEN=${DISCORD_APP_AUTH_TOKEN} YOUTUBE_API_KEY=${YOUTUBE_API_KEY} node ./bot.js
 done
