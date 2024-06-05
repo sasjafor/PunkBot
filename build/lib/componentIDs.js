3850485d869168f572933d6eefa8556cb895bf27
Sascha Forster <sascha.forster@hotmail.ch>
2024-06-05 20:52:16 +0200

    remove cached build files

--- build/lib/componentIDs.js
+++
@@ -1,9 +0,0 @@
-const buttons = {
-    queuePrev: 'queue_button_0',
-    queueNext: 'queue_button_1',
-};
-const selectMenus = {
-    searchMenu: 'search_select_menu_0',
-};
-export { buttons, selectMenus, };
-//# sourceMappingURL=componentIDs.js.map
